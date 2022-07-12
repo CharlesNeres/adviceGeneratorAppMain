f4ae7f7d3faab231a5892bdf2ea3cd7f84d0d3fe
Charles Júnior <charlesbritoneresjr@gmail.com>
2022-07-12 13:12:03 +0800

    created basic html structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPause } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <div className="App">      
+      <header>
+        <span>advice</span>
+        <span id="numberAdvice">#177</span>
+      </header>
+
+      <main id="contentAdvice">
+        "It is easy to sit up and take notice, whta's difficult is
+        getting up and taking action."
+      </main>
+
+      <footer>
+        <hr className="leftLine" />
+        <FontAwesomeIcon icon={faPause} />
+        <hr className="rightLine" />
+      </footer>
+    </div>    
   );
 }
 

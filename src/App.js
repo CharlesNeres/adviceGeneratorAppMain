0776e014510113de95eff7ddff7b2d3c55b388dd
Charles Júnior <charlesbritoneresjr@gmail.com>
2022-07-12 21:39:37 +0800

    added fetch api

--- src/App.js
+++ src/App.js
@@ -1,31 +1,67 @@
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPause, faDiceFive } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">      
-      <header>
-        <span>advice </span>
-        <span id="numberAdvice">#177</span>
-      </header>
+class App extends React.Component{
+  
+  constructor(props){
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.loadData = this.loadData.bind(this);
+    this.fetchAPI = this.fetchAPI.bind(this);
+    this.state = {}
+  }
 
-      <main>
-        "It is easy to sit up and take notice, whta's difficult is
-        getting up and taking action."
-      </main>
+  fetchAPI(){
+    fetch('https://api.adviceslip.com/advice')
+    .then(response=>response.json())
+    .then(data=>this.loadData(data.slip))
+    .catch(error=>console.error(error));
+    
+  } 
 
-      <footer>
-        <hr/>
-        <FontAwesomeIcon className="iconPause" icon={faPause} />
-        <hr/>
-      </footer>
+  handleClick(e){
+    this.fetchAPI();
+  }
 
-      <button>
-        <FontAwesomeIcon className="iconDice" icon={faDiceFive} />
-      </button>
-    </div>    
-  );
+  componentDidMount(){
+    console.log("testando mount")
+    this.fetchAPI();
+  }
+
+  loadData(data){
+    console.log("eteasfd")
+    this.setState({
+      id: data.id,
+      advice: data.advice
+    })
+  }
+
+  render(){    
+    return(
+      <div className="App">      
+         <header>
+           <span>advice </span>
+           <span id="numberAdvice">#{this.state.id}</span>
+         </header>
+
+         <main>
+           {this.state.advice}
+         </main>
+
+         <footer>
+           <hr/>
+           <FontAwesomeIcon className="iconPause" icon={faPause} />
+           <hr/>
+         </footer>
+
+         <button onClick={this.handleClick}>
+           <FontAwesomeIcon className="iconDice" icon={faDiceFive} />
+         </button>
+       </div>
+    )
+  }
 }
 
 export default App;

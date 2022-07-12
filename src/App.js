135df43067988ff1ea9fc93ada877beb20de2888
Charles Júnior <charlesbritoneresjr@gmail.com>
2022-07-12 14:05:58 +0800

    added button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPause } from '@fortawesome/free-solid-svg-icons';
+import { faPause, faDiceFive } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
 function App() {
   return (
     <div className="App">      
       <header>
-        <span>advice</span>
+        <span>advice </span>
         <span id="numberAdvice">#177</span>
       </header>
 
-      <main id="contentAdvice">
+      <main>
         "It is easy to sit up and take notice, whta's difficult is
         getting up and taking action."
       </main>
 
       <footer>
-        <hr className="leftLine" />
-        <FontAwesomeIcon icon={faPause} />
-        <hr className="rightLine" />
+        <hr/>
+        <FontAwesomeIcon className="iconPause" icon={faPause} />
+        <hr/>
       </footer>
+
+      <button>
+        <FontAwesomeIcon className="iconDice" icon={faDiceFive} />
+      </button>
     </div>    
   );
 }
